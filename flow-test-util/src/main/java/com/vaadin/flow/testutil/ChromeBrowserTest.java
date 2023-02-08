74424c9c90f1b85461bbd6a7df59ba8ba5d62498
Artur Signell <artur@vaadin.com>
2023-02-08 11:54:20 +0200

    test: Use --headless=new as ChromeOptions recommends for Chrome 109+

--- flow-test-util/src/main/java/com/vaadin/flow/testutil/ChromeBrowserTest.java
+++ flow-test-util/src/main/java/com/vaadin/flow/testutil/ChromeBrowserTest.java
@@ -154,7 +154,7 @@ public class ChromeBrowserTest extends ViewOrUITest {
 
     static ChromeOptions createHeadlessChromeOptions() {
         final ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--disable-gpu");
+        options.addArguments("--headless=new", "--disable-gpu");
         return options;
     }
 }
