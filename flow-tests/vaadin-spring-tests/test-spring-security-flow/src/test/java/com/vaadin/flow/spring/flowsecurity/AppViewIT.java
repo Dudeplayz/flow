3b7145e428ee08b759469bfe697bce099502c15d
Artur Signell <artur@vaadin.com>
2023-02-17 10:56:37 +0200

    .

--- flow-tests/vaadin-spring-tests/test-spring-security-flow/src/test/java/com/vaadin/flow/spring/flowsecurity/AppViewIT.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-flow/src/test/java/com/vaadin/flow/spring/flowsecurity/AppViewIT.java
@@ -35,93 +35,6 @@ public class AppViewIT extends AbstractIT {
     private void clickLogout() {
         getMainView().$(ButtonElement.class).id("logout").click();
     }
-/*
-    @Test
-    public void menu_correct_for_anonymous() {
-        open("");
-        List<MenuItem> menuItems = getMenuItems();
-        List<MenuItem> expectedItems = new ArrayList<MenuItem>();
-        expectedItems.add(new MenuItem("", "Public", true));
-        expectedItems.add(new MenuItem("private", "Private", false));
-        expectedItems.add(new MenuItem("admin", "Admin", false));
-        Assert.assertEquals(expectedItems, menuItems);
-    }
-
-    @Test
-    public void menu_correct_for_user() {
-        open(LOGIN_PATH);
-        loginUser();
-        List<MenuItem> menuItems = getMenuItems();
-        List<MenuItem> expectedItems = new ArrayList<MenuItem>();
-        expectedItems.add(new MenuItem("", "Public", true));
-        expectedItems.add(new MenuItem("private", "Private", true));
-        expectedItems.add(new MenuItem("admin", "Admin", false));
-        Assert.assertEquals(expectedItems, menuItems);
-    }
-
-    @Test
-    public void menu_correct_for_admin() {
-        open(LOGIN_PATH);
-        loginAdmin();
-        List<MenuItem> menuItems = getMenuItems();
-        List<MenuItem> expectedItems = new ArrayList<MenuItem>();
-        expectedItems.add(new MenuItem("", "Public", true));
-        expectedItems.add(new MenuItem("private", "Private", true));
-        expectedItems.add(new MenuItem("admin", "Admin", true));
-        Assert.assertEquals(expectedItems, menuItems);
-    }
-
-    @Test
-    public void root_page_does_not_require_login() {
-        open("");
-        assertRootPageShown();
-    }
-
-    @Test
-    public void other_public_page_does_not_require_login() {
-        open("another");
-        assertAnotherPublicPageShown();
-    }
-
-    @Test
-    public void navigate_to_private_view_prevented() {
-        open("");
-        navigateTo("private", false);
-        assertLoginViewShown();
-    }
-
-    @Test
-    public void navigate_to_admin_view_prevented() {
-        open("");
-        navigateTo("admin", false);
-        assertLoginViewShown();
-    }
-
-    @Test
-    public void redirect_to_private_view_after_login() {
-        open("private");
-        assertPathShown(LOGIN_PATH);
-        loginUser();
-        assertPrivatePageShown(USER_FULLNAME);
-    }
-
-    @Test
-    public void redirect_to_private_view_after_navigation_and_login() {
-        open("");
-        navigateTo("private", false);
-        assertPathShown(LOGIN_PATH);
-        loginUser();
-        assertPrivatePageShown(USER_FULLNAME);
-    }
-
-    @Test
-    public void redirect_to_admin_view_after_login() {
-        open("admin");
-        assertPathShown(LOGIN_PATH);
-        loginAdmin();
-        assertAdminPageShown(ADMIN_FULLNAME);
-    }
-*/
 
     @Test
     public void private_page_logout_should_redirect_to_root() {
@@ -131,7 +44,7 @@ public class AppViewIT extends AbstractIT {
         clickLogout();
         assertRootPageShown();
     }
-/* 
+
     @Test
     public void logout_redirects_to_root_page() {
         open(LOGIN_PATH);
@@ -141,7 +54,7 @@ public class AppViewIT extends AbstractIT {
         clickLogout();
         assertRootPageShown();
     }
-  */  @Test
+    @Test
     public void redirect_to_resource_after_login() {
         String contents = "Secret document for admin";
         String path = "admin-only/secret.txt";
@@ -234,50 +147,6 @@ public class AppViewIT extends AbstractIT {
                 shouldBeTextFile.contains("Public document for all users"));
         checkForBrowserErrors();
     }
-
-    // @Test
-    // public void upload_file_in_private_view() throws IOException {
-    //     open("private");
-    //     loginUser();
-    //     UploadElement upload = $(UploadElement.class).first();
-    //     File tmpFile = File.createTempFile("security-flow-image", ".png");
-    //     InputStream imageStream = getClass().getClassLoader()
-    //             .getResourceAsStream("image.png");
-    //     IOUtils.copyLarge(imageStream, new FileOutputStream(tmpFile));
-    //     tmpFile.deleteOnExit();
-    //     upload.upload(tmpFile);
-
-    //     TestBenchElement text = $("p").id("uploadText");
-    //     TestBenchElement img = $("img").id("uploadImage");
-
-    //     Assert.assertEquals("Loan application uploaded by John the User",
-    //             text.getText());
-    //     Assert.assertTrue(img.getPropertyString("src")
-    //             .contains("/VAADIN/dynamic/resource/"));
-    // }
-
-    // @Test
-    // public void navigate_in_thread_without_access() {
-    //     open("");
-    //     $(ButtonElement.class).id(PublicView.BACKGROUND_NAVIGATION_ID).click();
-
-    //     // This waits for longer than the delay in the UI so we do not need a
-    //     // separate
-    //     // sleep
-    //     assertLoginViewShown();
-    // }
-
-    // @Test
-    // public void navigate_in_thread_with_access() {
-    //     open(LOGIN_PATH);
-    //     loginAdmin();
-    //     $(ButtonElement.class).id(PublicView.BACKGROUND_NAVIGATION_ID).click();
-
-    //     // This waits for longer than the delay in the UI so we do not need a
-    //     // separate
-    //     // sleep
-    //     assertAdminPageShown(ADMIN_FULLNAME);
-    // }
 
     private void navigateTo(String path) {
         navigateTo(path, true);
